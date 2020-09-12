bd87047c7dd4915ca95feedbfe52120913d963f4
wheelsandmetal <jakob@schmutz.co.uk>
2020-09-12 15:30:00 +0100

    fix(font): make smaller

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "FiraCode Nerd Font:style=Regular:size=11";
+static char *font = "FiraCode Nerd Font:style=Regular:size=10";
 static int borderpx = 2;
 
 /*
